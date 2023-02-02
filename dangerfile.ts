552bdf4e949bb1a10e0794b40f31405326bfcc34
Hayuna <marek.kowalonek@gmail.com>
2023-02-02 21:32:23 +0100

    init

---
+++ dangerfile.ts
@@ -0,0 +1,23 @@
+import { danger, warn, fail } from "danger";
+
+const reviewLargePR = () => {
+  const bigPRThreshold = 300;
+  if (
+    danger.github.pr.additions + danger.github.pr.deletions >
+    bigPRThreshold
+  ) {
+    warn(
+      `:exclamation: Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR for faster, easier review.`
+    );
+  }
+};
+
+const ensurePRHasAssignee = () => {
+  if (danger.github.pr.assignee === null) {
+    fail(
+      `Please assign someone to merge this PR, and optionally include people who should review`
+    );
+  }
+};
+
+reviewLargePR();
