5b4c8f721679d6d5278c4b1a0cb21890564da264
Hayuna <marek.kowalonek@gmail.com>
2023-02-04 22:20:11 +0100

    .

--- scripts/combineDangerfile.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-
-fs.readdirSync("./danger-rules").forEach((file) => {
-  console.log(file);
-  fs.appendFileSync(
-    "./dangerfile.ts",
-    fs.readFileSync(`./danger-rules/${file}`).toString()
-  );
-});
