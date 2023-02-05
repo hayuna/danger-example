dd52a9f5b15969c470a9b39a2670189ba3aa049f
Hayuna <marek.kowalonek@gmail.com>
2023-02-05 18:03:37 +0100

    .

--- danger-rules/noConsole.ts
+++ danger-rules/noConsole.ts
@@ -4,12 +4,6 @@ const PATTERN = /console\.(log|error|warn|info)/;
 const GLOBAL_PATTERN = new RegExp(PATTERN.source, "g");
 const JS_FILE = /\.(js|ts)x?$/i;
 
-// interface FindConsoleResponse {
-//   file: string;
-//   lineNumber: number;
-//   whichConsole: string;
-// }
-
 const findConsole = (file, content, whitelist) => {
   const lines = content.split("\n");
   console.log(lines);
@@ -17,15 +11,10 @@ const findConsole = (file, content, whitelist) => {
   lines.forEach((line, lineNumber) => {
     let matches = line.match(PATTERN);
     if (matches) {
-      console.log({ type: matches[0], lineNumber, file });
-      // const consoles = matches.filter((match) => {
-      //   const singleMatch = PATTERN.exec(match);
-      //   if (!singleMatch || singleMatch.length === 0) return false;
-      //   match.lineNumber = lineNumber;
-      //   return !whitelist.includes(singleMatch[1]);
-      // });
-      // @ts-ignore
-      // response.push(consoles|);
+      if (!whitelist.includes(matches[1])) {
+        // @ts-ignore
+        response.push({ type: matches[0], lineNumber, file });
+      }
     }
   });
 
@@ -36,8 +25,8 @@ const isFileInDangerRules = (file) => {
   return file.includes("danger-rules/");
 };
 
-const defaultCallback = (file, matches) => {
-  warn(`${matches} console statement(s) added in ${file}.`);
+const defaultCallback = (matches) => {
+  warn(`${matches.file}:${matches.lineNumber} - ${matches.type} found`);
 };
 
 const noConsole = async ({
@@ -61,7 +50,7 @@ const noConsole = async ({
         if (diff) {
           const matches = findConsole(file, diff.added, whitelist);
           if (matches.length === 0) return;
-          callback(file, matches.length);
+          callback(matches);
         }
       }
     });
