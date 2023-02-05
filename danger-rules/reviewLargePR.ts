35626feaeeb35d5db5961c1f59d72183f996b23d
Hayuna <marek.kowalonek@gmail.com>
2023-02-06 00:45:12 +0100

    .

--- danger-rules/reviewLargePR.ts
+++ danger-rules/reviewLargePR.ts
@@ -1,6 +1,6 @@
 import { danger, warn } from "danger";
 
-const reviewLargePR = () => {
+export const reviewLargePR = () => {
   const bigPRThreshold = 300;
   if (
     danger.github.pr.additions + danger.github.pr.deletions >
