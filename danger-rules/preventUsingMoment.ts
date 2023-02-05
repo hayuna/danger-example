2e07c85aebddc7c9e39e0100d8d1019d69373262
Hayuna <marek.kowalonek@gmail.com>
2023-02-05 22:06:20 +0100

    .

---
+++ danger-rules/preventUsingMoment.ts
@@ -0,0 +1,27 @@
+import { danger, warn } from "danger";
+import * as fs from "fs";
+
+const filesOnly = (file: string) =>
+  fs.existsSync(file) && fs.lstatSync(file).isFile();
+const getCreatedFileNames = (createdFiles: string[]) =>
+  createdFiles.filter(filesOnly);
+
+const preventUsingMoment = () => {
+  const newMomentImports = getCreatedFileNames(danger.git.created_files).filter(
+    (filename) => {
+      const content = fs.readFileSync(filename).toString();
+      return (
+        content.includes('from "moment"') ||
+        content.includes('from "moment-timezone"')
+      );
+    }
+  );
+  if (newMomentImports.length > 0) {
+    warn(`We are trying to migrate away from moment towards \`luxon\`, but found moment imports in the following new files:
+  ${newMomentImports.map((filename) => `- \`${filename}\``).join("\n")}
+  See [docs](https://moment.github.io/luxon/api-docs/index.html).
+    `);
+  }
+};
+
+preventUsingMoment();
