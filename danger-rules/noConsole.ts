a9cd8baac62e71e5d4e70a2a480b4e664d797422
Hayuna <marek.kowalonek@gmail.com>
2023-02-05 15:00:22 +0100

    .

--- danger-rules/noConsole.ts
+++ danger-rules/noConsole.ts
@@ -4,20 +4,31 @@ const PATTERN = /console\.(log|error|warn|info)/;
 const GLOBAL_PATTERN = new RegExp(PATTERN.source, "g");
 const JS_FILE = /\.(js|ts)x?$/i;
 
+// interface FindConsoleResponse {
+//   file: string;
+//   lineNumber: number;
+//   whichConsole: string;
+// }
+
 const findConsole = (content, whitelist) => {
   const lines = content.split("\n");
   console.log(lines);
-  let matches = content.match(GLOBAL_PATTERN);
-
-  if (!matches) return [];
-
-  const consoles = matches.filter((match) => {
-    const singleMatch = PATTERN.exec(match);
-    if (!singleMatch || singleMatch.length === 0) return false;
-    return !whitelist.includes(singleMatch[1]);
+  const response = [];
+  lines.forEach((line, lineNumber) => {
+    let matches = line.match(PATTERN);
+    if (matches) {
+      const consoles = matches.filter((match) => {
+        const singleMatch = PATTERN.exec(match);
+        if (!singleMatch || singleMatch.length === 0) return false;
+        match.lineNumber = lineNumber;
+        return !whitelist.includes(singleMatch[1]);
+      });
+      // @ts-ignore
+      response.push(consoles);
+    }
   });
 
-  return consoles;
+  return response;
 };
 
 const isFileInDangerRules = (file) => {
@@ -32,6 +43,10 @@ const noConsole = async ({
   whitelist = [],
   callback = defaultCallback,
 } = {}) => {
+  console.log({
+    created_files: danger.git.created_files,
+    modified_files: danger.git.modified_files,
+  });
   const diffs = [...danger.git.created_files, ...danger.git.modified_files]
     .filter((file) => JS_FILE.test(file))
     .map((file) => {
@@ -45,6 +60,7 @@ const noConsole = async ({
     .forEach(({ file, diff }) => {
       if (!isFileInDangerRules(file)) {
         if (diff) {
+          console.log({ added: diff.added });
           const matches = findConsole(diff.added, whitelist);
           if (matches.length === 0) return;
           callback(file, matches.length);
