ace56eac4f74abc548a3b44d3e1750a9e1c36f65
Hayuna <marek.kowalonek@gmail.com>
2023-02-08 20:05:16 +0100

    .

--- danger-rules/updateVersion.ts
+++ danger-rules/updateVersion.ts
@@ -3,15 +3,7 @@ import { danger, warn } from "danger";
 const updateVersion = () => {
   console.log(`started updateVersion`);
   danger.git.JSONDiffForFile("package.json").then((packageDiff) => {
-    console.log({
-      before: packageDiff.version.before,
-      after: packageDiff.version.after,
-      compare: packageDiff.version.before === packageDiff.version.after,
-    });
-    if (
-      packageDiff.version &&
-      packageDiff.version.before === packageDiff.version.after
-    ) {
+    if (!packageDiff.version) {
       warn("Shouldn't you update package version?");
     }
   });
